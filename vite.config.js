b21e532561efc7e2db3767ec2ae41e25e1955c7a
Viacheslav Plevako <cheryec@ukr.net>
2024-02-07 19:21:26 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+  if (command !== "serve") {
+    config.base = "/test_swiper/";
+  }
+  return config;
+});
